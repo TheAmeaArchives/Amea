eaa937cc5a584b326401bb7f857e092edb506795
code-env <org.codeen@gmail.com>
2024-06-24 21:29:29 +0100

    feat: fixed the missing key props

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
       <div className="h-20 max-width  xl:p-0 px-4">
         <div className="h-full w-full flex items-center justify-center gap-5">
           {socialLinks.map((link) => (
-            <Link href={link.url} target="_blank">
+            <Link href={link.url} target="_blank" key={link.name}>
               <Image src={link.path} alt={link.name} width={20} height={20} />
             </Link>
           ))}

2eeaf4838474c1f0eca38bfbf20ea681dac00016
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-07-22 01:05:40 +0100

    fix: fixed blog, gallery and team page ui bug

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -11,19 +11,19 @@ const Gallery = () => {
         {/* <h1 className="text-4xl font-bold">The Grand Galery</h1> */}
       </div>
       <div className="h-screen center ">
-        <h1 className="text-[32px] font-normal text-center">
+        <h1 className="text-[32px] max-md:text-xl font-normal text-center">
           Display of projects built & impact <br /> created using our insights.
         </h1>
       </div>
       <div className="h- center ">
-        <div className="h-full w-full  grid grid-cols-3 gap-4 p-5">
+        <div className="h-full w-full  md:grid md:grid-cols-3 md:gap-4 max-md:space-y-11 p-5">
           {Array.from({ length: 6 }).map((_, index) => (
             <div className="flex flex-col gap-3" key={index}>
               <div className="bg-default w-full h-56 rounded-lg" />
               <div>
                 <h1 className="text-xl font-medium">Title</h1>
                 <p className="text-sm text-black/70">Lorem ipsum dolor sit.</p>
-                <p className="text-sm text-black/70">Lorem</p>
+                <p className="text-sm text-black/70 max-md:hidden">Lorem</p>
               </div>
             </div>
           ))}

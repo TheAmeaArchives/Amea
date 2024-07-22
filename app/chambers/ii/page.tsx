1da9191af117bec8b2bc3b170f7adcca9987e828
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-07-22 10:01:26 +0100

    fix: fixed chambers i, ii and iii pages ui bug

--- app/chambers/ii/page.tsx
+++ app/chambers/ii/page.tsx
@@ -10,22 +10,22 @@ const ChamberTwo = () => {
         title="chamber ii"
         description="A Virtual Research Center."
       />
-      <div className="flex flex-col gap-10 items-center bg">
-        <div className="h-full flex w-full items-center justify-evenly">
+      <div className="flex flex-col gap-y-[70px] items-center bg">
+        <div className="h-full flex w-full items-center md:justify-evenly">
           {stats.map((stat) => (
             <div key={stat.text} className="text-center">
-              <h2 className="editor-font text-5xl font-bold">{stat.count}</h2>
-              <p className="max-w-32 text-center text-2xl aileron font-light mt-2">
+              <h2 className="editor-font text-5xl max-md:text-4xl font-bold">{stat.count}</h2>
+              <p className="max-w-32 text-center text-2xl max-md:text-xl aileron font-light mt-2">
                 {stat.text}
               </p>
             </div>
           ))}
         </div>
-        <div className="flex flex-col gap-2 items-center">
+        <div className="flex flex-col gap-2 items-center gap-y-20 md:gap-y-10">
           <div className="w-1 bg-black h-60 rounded" />
           <Link
             href="/chambers/i/volunteer"
-            className="text-center text-default text-2xl hover:underline"
+            className="text-center w-[110px] md:w-[185px] text-default text-2xl max-md:text-lg pb-2 border-b-2 border-default"
           >
             Volunteer to participate
           </Link>
@@ -34,8 +34,8 @@ const ChamberTwo = () => {
       <div className="flex flex-col gap-20 mt-20">
         {constants.map((constant) => (
           <div key={constant.name} className="flex flex-col gap-6">
-            <h1 className="text-5xl font-bold akira">{constant.name}</h1>
-            <p className="text-xl font-light">{constant.description}</p>
+            <h1 className="text-5xl max-md:text-2xl font-bold akira">{constant.name}</h1>
+            <p className="text-xl max-md:text-base font-light">{constant.description}</p>
           </div>
         ))}
       </div>

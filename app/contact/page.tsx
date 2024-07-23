d19c447787e50e4e33d46e75526261367cecb39d
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-07-23 21:30:30 +0100

    fix: fixed contact page responsive ui bug

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="min-h-screen w-full lg:center flex-col lg:flex-row ">
-      <div className="flex-[1.5] gap-3">
-        <h1 className="text-5xl font-bold akira">CONTACT US</h1>
+      <div className="flex-[1.5] gap-3 mb-8">
+        <h1 className="text-5xl max-md:text-2xl font-bold akira">CONTACT US</h1>
       </div>
       <div className="flex-[2]">
         <ContactForm />
